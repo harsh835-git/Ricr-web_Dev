798211843edbfceb4879c738a7e8a90dcb4938f9
harsh835-git <harshsoni83588@gmail.com>
2025-11-14 20:27:48 +0530

    JavaScriptBasic

---
+++ JavaScript/index.js
@@ -0,0 +1,20 @@
+var a = 10;
+var a = 20;
+
+let b = 10;
+console.log(a, b);
+
+let ar = ["abc", 1234, 45.65, 'a', true]
+console.log(ar)
+
+
+console.warn("This is warning")
+console.error("This is error")
+alert("This is alert")
+
+if(confirm("Are you sure")){
+    console.log("thanks");
+}
+else{
+    console.log("Its ok")
+}
