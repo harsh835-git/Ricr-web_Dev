28062f5f7abd09f6f62dcaff5598c53b3c8cb07a
harsh835-git <harshsoni83588@gmail.com>
2025-12-06 22:50:00 +0530

    RainbowBulb

--- JavaScript/EventsInJS/script.js
+++ JavaScript/EventsInJS/script.js
@@ -41,6 +41,7 @@ function SB_control() {
     }
 }
 
+
 function SB_control2() {
     document.getElementById("smartbulb").classList.toggle("on");
 }
@@ -48,35 +49,64 @@ function SB_control2() {
 function fillColor(Rcolor) {
     document.getElementById("Rainbowbulb").style.backgroundColor = Rcolor;
 }
-document.getElementById("c1").addEventListener("mouseenter"), () => {
+document.getElementById("c1").addEventListener("mouseenter", () => {
     fillColor("red")
-}
+});
 
-document.getElementById("c1").addEventListener("mouseenter"), () => {
-    fillColor("yellow")
-}
-
-document.getElementById("c1").addEventListener("mouseenter"), () => {
-    fillColor("green")
-}
-
-document.getElementById("c1").addEventListener("mouseenter"), () => {
-    fillColor("blue")
-}
-
-document.getElementById("c1").addEventListener("mouseenter"), () => {
-    fillColor("violet")
-}
-
-document.getElementById("c1").addEventListener("mouseenter"), () => {
-    fillColor("pink")
-}
-
-document.getElementById("c1").addEventListener("mouseenter"), () => {
-    fillColor("cyan")
-}
-
-
-document.getElementById("c1").addEventListener("mouseleave"), () => {
+document.getElementById("c1").addEventListener("mouseleave", () => {
     fillColor("white")
-}
+});
+
+
+document.getElementById("c2").addEventListener("mouseenter", () => {
+    fillColor("yellow")
+});
+
+document.getElementById("c2").addEventListener("mouseleave", () => {
+    fillColor("white")
+});
+
+
+document.getElementById("c3").addEventListener("mouseenter", () => {
+    fillColor("green")
+});
+document.getElementById("c3").addEventListener("mouseleave", () => {
+    fillColor("white")
+});
+
+
+document.getElementById("c4").addEventListener("mouseenter", () => {
+    fillColor("blue")
+});
+
+
+document.getElementById("c4").addEventListener("mouseleave", () => {
+    fillColor("white")
+});
+
+document.getElementById("c5").addEventListener("mouseenter", () => {
+    fillColor("violet")
+});
+
+document.getElementById("c5").addEventListener("mouseleave", () => {
+    fillColor("white")
+});
+
+
+document.getElementById("c6").addEventListener("mouseenter", () => {
+    fillColor("pink")
+});
+
+document.getElementById("c6").addEventListener("mouseleave", () => {
+    fillColor("white")
+});
+
+
+document.getElementById("c7").addEventListener("mouseenter", () => {
+    fillColor("cyan")
+});
+
+
+document.getElementById("c7").addEventListener("mouseleave", () => {
+    fillColor("white")
+});
