7c194af8c89927ae988179eda92898510cdc5033
harsh835-git <harshsoni83588@gmail.com>
2025-12-14 17:31:13 +0530

    WeatherApp

---
+++ JavaScript/WeatherApp/script.js
@@ -0,0 +1,30 @@
+async function Getweather() {
+    const city = document.getElementById("city").value.trim();
+    const { lat, lon } = await getGeoLoc(city);
+
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=f7f103e141b854a95fff96eec8a4ce58`)
+    const data = await response.json();
+
+    document.getElementById("WeatherData").innerHTML = `
+         <div>
+            <p>Temperature : ${(data.main.temp - 273.14).toFixed(2)}°C</p> 
+            <p>Humidity : ${data.main.humidity}%</p>
+            <p>Description : ${data.weather[0].description}</p>
+          </div>
+          <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png" alt="WeatherIcon" class="mx-5"/>
+        
+    `;
+
+}
+
+async function getGeoLoc(City) {
+    console.log(City);
+    const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${City}&limit=1&appid=f7f103e141b854a95fff96eec8a4ce58`);
+
+    const data = await response.json();
+    const lat = data[0].lat;
+    const lon = data[0].lon;
+
+    return { lat, lon };
+
+}
