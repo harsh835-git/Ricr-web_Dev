b7d4e5e74b98c80e4dbc1f5abf224bf2e4f92e93
harsh835-git <harshsoni83588@gmail.com>
2025-12-23 16:44:34 +0530

    ShoppingKart

---
+++ Assignment/Shoppingkart/script.js
@@ -0,0 +1,21 @@
+function validateCheckoutForm() {
+    const name = document.getElementById("name").value;
+    alert(`Thank you for Shopping, ${name}!`);
+    console.log("Checkout Successful:", name);
+    return false;
+}
+
+function validateContactForm() {
+    const name = document.getElementById("contactName").value;
+    const email = document.getElementById("contactEmail").value;
+    const message = document.getElementById("contactMessage").value;
+
+    if (!email.includes("@") || message.length < 10) {
+        alert("Please enter valid details.");
+        return false;
+    }
+
+    alert(`Thank you for contacting us, ${name}!`);
+    console.log({ name, email, message });
+    return false;
+}
