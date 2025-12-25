a01380279508b9c4d52b88009467ffafb491eb87
harsh835-git <harshsoni83588@gmail.com>
2025-12-25 14:48:56 +0530

    ReactInstall,FirstApp,Deploy

---
+++ React/Demo/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function App() {
+  let a = 5;
+  let b = 10;
+
+  console.log(a + b);
+
+  return (
+    <>
+      <h1 id="head">Hello World</h1>
+      <div>This Is my first React App</div>
+    </>
+  );
+}
+
+
+export default App;
