9c233351e689abf39e6e213717088eef9267475b
harsh835-git <harshsoni83588@gmail.com>
2025-12-26 19:58:08 +0530

    ComponentsOfReact

--- React/Demo/src/App.jsx
+++ React/Demo/src/App.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 function App() {
-  let a = 5;
-  let b = 10;
-
-  console.log(a + b);
-
   return (
     <>
-      <h1 id="head">Hello World</h1>
-      <div>This Is my first React App</div>
+    <Header/>
+      <div className="bg-black text-light text-capitalize text-center fs-2 fw-bolder p-1 ">
+        This Is my first React App
+      </div>
+
+      <Footer/>
     </>
   );
 }
-
 
 export default App;
---
+++ React/Demo/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <h1 id="head" className="text-center bg-black p-2 shadow">
+        <i className="bi bi-x-octagon-fill"></i> Hello World{" "}
+        <i className="bi bi-x-octagon-fill"></i>{" "}
+      </h1>
+    </>
+  );
+};
+export default Header;
