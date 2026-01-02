a0b75567f26df8e8fd44417b0b531c5d6fee231a
harsh835-git <harshsoni83588@gmail.com>
2026-01-02 20:06:09 +0530

    CurrencyConverter

--- React/CurrencyConverter/src/App.jsx
+++ React/CurrencyConverter/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/Header";
 import Currency from "./components/Currency";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
   return (
     <>
+      <Toaster />
       <Header />
       <Currency />
     </>
--- React/CurrencyConverter/src/components/Currency.jsx
+++ React/CurrencyConverter/src/components/Currency.jsx
@@ -1,33 +1,110 @@
 import React, { useState } from "react";
 import CountryData from "../assets/CountryData.json";
+import toast from "react-hot-toast";
+import axios from "axios";
 
 const Currency = () => {
   const [from, setFrom] = useState("");
   const [to, setTo] = useState("");
-  console.log(CountryData);
+  const [fromAmt, setFromAmt] = useState("");
+  const [toAmt, setToAmt] = useState("");
+
+  const Convert = async () => {
+    if (!from || !to || !fromAmt) {
+      toast.error("Some Fields Missing");
+      return;
+    }
+    try {
+      const res = await axios.get(
+        `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${from
+          .split(" ")[0]
+          .toLowerCase()}.json`
+      );
+
+      
+      setToAmt(fromAmt*res.data[from.split(" ")[0].toLowerCase()][to.split(" ")[0].toLowerCase()]);
+      
+    } catch (error) {}
+  };
 
   return (
     <>
-      <div className="bg-amber-100 h-screen">
-        <div className=" w-3xl bg-white rounded shaow border p-3">
-          <div className="grid gird-cols-2">
-            <select
-              name="from"
-              value={from}
-              onChange={(e) => setFrom(e.target.value)}
-              className="border p-3 rounded overflow-hidden"
-            >
-              <option value="">-Select Country-</option>
-            </select>
+      <div className="bg-amber-50 h-screen p-5">
+        <div className="w-3xl bg-white rounded shadow border p-3 mx-auto space-y-5">
+          <div className="grid grid-cols-2 gap-5">
+            <div className="flex gap-3 border rounded px-3">
+              {from && (
+                <img
+                  src={`https://flagsapi.com/${from.split(" ")[1]}/flat/48.png`}
+                  alt=""
+                />
+              )}
+              <select
+                name="from"
+                value={from}
+                onChange={(e) => setFrom(e.target.value)}
+                className=" p-3 overflow-hidden w-full focus:outline-none"
+              >
+                <option value="">-Select Country-</option>
+                {CountryData.map((country, idx) => (
+                  <option
+                    value={country.CurrencyCode + " " + country.CountryCode}
+                    key={idx}
+                  >
+                    {country.CountryName}
+                  </option>
+                ))}
+              </select>
+            </div>
 
-            <select
-              name="to"
-              value={to}
-              onChange={(e) => setto(e.target.value)}
-              className="border p-3 rounded overflow-hidden"
-            >
-              <option value="">-Select Country-</option>
-            </select>
+            <div className="flex gap-3 border rounded px-3">
+              {to && (
+                <img
+                  src={`https://flagsapi.com/${to.split(" ")[1]}/flat/48.png`}
+                  alt=""
+                />
+              )}
+              <select
+                name="to"
+                value={to}
+                onChange={(e) => setTo(e.target.value)}
+                className=" p-3 overflow-hidden w-full focus:outline-none"
+              >
+                <option value="">-Select Country-</option>
+                {CountryData.map((country, idx) => (
+                  <option
+                    value={country.CurrencyCode + " " + country.CountryCode}
+                    key={idx}
+                  >
+                    {country.CountryName}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
+          <div className="flex gap-3 items-center">
+            <label htmlFor="fromAmt">Amount</label>
+            <input
+              type="text"
+              name="fromAmt"
+              value={fromAmt}
+              onChange={(e) => setFromAmt(e.target.value)}
+              placeholder="Enter the Amount to Convert"
+              className="border rounded p-3 w-full"
+            />
+          </div>
+
+          <button
+            className="bg-green-300 text-green-900 hover:bg-green-600 hover:text-white px-4 py-2 border rounded hover:shadow-md w-full"
+            onClick={Convert}
+          >
+            Convert
+          </button>
+
+          <div className="border" />
+
+          <div className="flex gap-3 items-center">
+            <label htmlFor="toAmt">Converted Amount : {toAmt?toAmt:"XXXXXX"}</label>
           </div>
         </div>
       </div>
