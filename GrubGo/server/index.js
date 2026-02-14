032ce0620433b517b0b99d106f27c80115040144
harsh835-git <harshsoni83588@gmail.com>
2026-02-14 18:30:22 +0530

    DummyAdmin

--- GrubGo/server/index.js
+++ GrubGo/server/index.js
@@ -17,7 +17,6 @@ app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(morgan("dev"));
-
 app.use("/auth", AuthRouter);
 app.use("/public", PublicRouter);
 app.use("/user", UserRouter);
